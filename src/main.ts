bccd114e6e1ec8c64d05f9f408bf777a12d92cef
saleem92 <salimmalikn@gmail.com>
2025-01-02 15:33:43 +0100

    init serverless for nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
 }
-bootstrap();
+
+bootstrap().then(() => {
+  console.log('NestJS app initialized');
+});
+
+export default server;
